8dea8d62573bce9412e672d4b082797401e8212b
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-09-12 18:33:18 -0500

    Finish p-write.php

--- includes/process/p-write.php
+++ includes/process/p-write.php
@@ -1,2 +1,23 @@
 <?php
 
+$h = new Helper();
+$msg = '';
+$username = $_POST['username'];
+$is_admin = $_POST['is_admin'];
+
+if ($h->isEmpty([$username, $is_admin])) {
+    header("Location admin.php");
+} else {
+    if ($_POST['submit']) {
+        $title = $_POST['title'];
+        $post = $_POST['post'];
+        $audience = $_POST['audience'];
+        if ($h->isEmpty([$title, $post, $audience])) {
+            $msg = "All fields required";
+        } else {
+            $admin = new Admin($username);
+            $admin->insertIntoPostDB($title, $post, $audience);
+            $msg = "Message saved successfully";
+        }
+    }
+}
