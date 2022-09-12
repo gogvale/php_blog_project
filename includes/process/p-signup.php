1a17ce9f037427b0fc41b8a5c5db84ac73b9b2cb
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-09-12 18:10:37 -0500

    Finish p-signup.php

--- includes/process/p-signup.php
+++ includes/process/p-signup.php
@@ -1,2 +1,30 @@
 <?php
 
+$h = new Helper();
+$msg = '';
+$username = '';
+
+if ($_POST['submit']) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    if ($h->isEmpty([$_POST['username'], $_POST['password'], $_POST['confirm_password']])) {
+        $msg = "All fields required";
+    } else if (!$h->isValidLength($username,6,100)) {
+        $msg = "Username should be between 6 and 100 characters";
+    } else if (!$h->isValidLength($password)) {
+        $msg = "Password should be between 8 and 20 characters";
+    } else if (!$h->isSecure($password)) {
+        $msg = "Password should contain at least 1 lowercase character, 1 uppercase character and 1 digit";
+    } else if (!$h->passwordsMatch($password,$confirm_password)) {
+        $msg = "Passwords don't match";
+    } else {
+        $bm = new BlogMember($username);
+        if($bm->isDuplicateID()){
+            $msg = "Username already exists";
+        }else{
+            $bm->insertIntoMemberDB($_POST['password']);
+            header("Location: index.php?new=1");
+        }
+    }
+}
