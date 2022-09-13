e4bb8ef4237e0a422fedd948ab05438543eafebe
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-09-12 20:36:53 -0500

    Conditional logout links

--- logout.php
+++ logout.php
@@ -1,10 +1,20 @@
+<?php
+session_start();
+session_destroy();
+include "UI_include.php";
+include "includes/header.html";
+?>
+
 <body>
     <div class="logoutsuccess">
         <div class="card read-card">
             <div class="card-body">
                 <p>You have logged out successfully</p>
+                <?php if(isset($_GET['admin'])): ?>
                     <div><a href = 'admin.php'>Click here to log in again</a></div>
-                    <div><a href = 'index.php'>Click here to log in again</a></div>                
+                <?php else: ?>
+                    <div><a href = 'index.php'>Click here to log in again</a></div>
+                <?php endif; ?>
             </div>
         </div>
 
