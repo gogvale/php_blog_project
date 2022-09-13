82146ac2b7402657966a4faa1fb26192cdbbe72b
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-09-12 19:00:12 -0500

    Finish messagecard.php

--- includes/process/output_code/messagecard.php
+++ includes/process/output_code/messagecard.php
@@ -1,13 +1,20 @@
+<?php /** @noinspection ALL */?>
 <div class="card read-card">
     <div class="card-body">
 
-        <h4 class="card-title">"post_title"</h4>
+        <h4 class="card-title"><?= $title ?></h4>
         <hr>
+
+        <?php
+        if ($is_member and $lastPost < $msgid)
+        {
+            echo '<span class = "new-post">NEW</span>';
+            $update = true;
+        }
+        ?>
         
-        //add PHP code here        
-        
-        <span class="post-time">Posted by "user_name" on "post_date" (Eastern Time)</span>
-        <p class="card-text">"post_text"</p>            
+        <span class="post-time">Posted by <?= $username ?> on <?= date($postdate) ?> (Eastern Time)</span>
+        <p class="card-text"><?= $post ?></p>
 
     </div>
 </div> 
